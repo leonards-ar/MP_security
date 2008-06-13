8b194c8828bf5814ee14e0bccbd62a824fbb8a91
Federico Barrera Oro <federico.barreraoro@mulesoft.com>
2008-06-13 12:13:22 +0000

    Errors Descriptors added.

--- src/main/java/com/mindpool/security/service/UserAuthenticationService.java
+++ src/main/java/com/mindpool/security/service/UserAuthenticationService.java
@@ -1,15 +1,18 @@
 package com.mindpool.security.service;
 
-import java.util.Set;
+import java.security.Principal;
 
-import com.mindpool.security.principal.SecurityUser;
+import javax.security.auth.login.LoginException;
 
 public interface UserAuthenticationService {
 
-	public SecurityUser authenticateUser(String username, String password) throws Exception;
+	public static final String UNKNOWN_USER_ERROR = "UNKNOWN_USER_ERROR";
+	public static final String USER_DISABLED_ERROR = "USER_DISABLED_ERROR";
+	public static final String PASSWORD_CHANGE_REQUIRED = "PASSWORD_CHANGE_REQUIRED";
+	public static final String PASSWORD_EXPIRED = "PASSWORD_EXPIRED";
+	public static final String BAD_PASSWORD = "BAD_PASSWORD";
 	
-	public Set<String>getRoles(SecurityUser user);
+	public Principal authenticate(String username, String password) throws LoginException;
+	public String getReason();
 	
-	public Set<String>getPermissions(SecurityUser user);
-		
 }
